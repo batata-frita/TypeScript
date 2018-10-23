84c06bb458c1b928d8d8e5f8441b0d2ded2b76ba
Sheetal Nandi <shkamat@microsoft.com>
2018-10-22 18:06:19 -0700

    Create ancestor projects for composite projects till projectRoot but load them and their references when doing all project wide operation like rename

--- src/server/project.ts
+++ src/server/project.ts
@@ -1443,7 +1443,7 @@ namespace ts.server {
         }
 
         /*@internal*/
-        forEachResolvedProjectReference<T>(
+        forEachProjectReference<T>(
             cb: (resolvedProjectReference: ResolvedProjectReference | undefined, resolvedProjectReferencePath: Path) => T | undefined,
             cbProjectRef: (projectReference: ProjectReference) => T | undefined,
             cbPotentialProjectRef: (path: Path) => T | undefined
