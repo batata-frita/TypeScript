e9ea222c4541f8032b9a9177c66763796282b0e7
psxcode <psxcode@gmail.com>
2019-06-04 10:11:22 +0200

    🚧 cleanup

--- tests/cases/conformance/types/typeRelationships/typeInference/unionAndIntersectionInference3.ts
+++
@@ -1,7 +0,0 @@
-// @strict: true
-
-// Repro from #30720
-
-type Maybe<T> = T | undefined;
-declare function concatMaybe<T>(...args: (Maybe<T> | Maybe<T>[])[]): T[];
-concatMaybe([1, 2, 3], 4);
--- tests/cases/fourslash/findAllRefsDeclareClass.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="fourslash.ts" />
-
-////declare class [|{| "isWriteAccess": true, "isDefinition": true |}C|] {
-////    static m(): void;
-////}
-
-verify.singleReferenceGroup("class C");
