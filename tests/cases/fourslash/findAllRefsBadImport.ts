61fb99afc5db8a5c04c7fe58c63ddfddc00b5558
psxcode <psxcode@gmail.com>
2019-06-19 16:15:41 +0200

    🚧 cleanup

--- tests/cases/fourslash/findAllRefsBadImport.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='fourslash.ts'/>
-
-////import { [|ab|] as [|{| "isWriteAccess": true, "isDefinition": true |}cd|] } from "doesNotExist";
-
-const [r0, r1]  = test.ranges();
-verify.referenceGroups(r0, undefined);
-verify.singleReferenceGroup("import cd", [r1]);
